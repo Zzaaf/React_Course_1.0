bb57d734ec6fd6bb27094d2b229a03907c528c56
Zzaaf <zzaafamania@gmail.com>
2019-01-21 22:54:18 +0300

    Добавил Babel в сборщик + новая структуру

---
+++ lesson_01_02/srcOrig/index.js
@@ -0,0 +1,12 @@
+import {employersNames} from './employersNames';
+import {money} from './calcCash';
+import {MakeBusiness} from './makeBusiness';
+
+// Экземпляр 
+const business = new MakeBusiness({
+    owner: 'Сэм',   
+    money: 0,
+    employersNames: employersNames
+});
+
+business.info();
